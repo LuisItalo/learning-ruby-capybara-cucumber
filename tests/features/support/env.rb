cd4a6c0cd06733454f09e13fe78e73fb10b0ab26
Luis Italo <luisitalomoreirasouza@gmail.com>
2022-09-05 10:34:25 -0300

    acessando url e verificando se esta correta

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -5,7 +5,7 @@ Capybara.configure do |config|
     #selenium    selenium_chrome    selenium_chrome_healess
 
     config.default_driver = :selenium_chrome
-    config.app_host = 'https://automacaocombatista.herokuapp.com/'
+    config.app_host = 'https://automacaocombatista.herokuapp.com'
     config.default_max_wait_time = 5
 
 end
